271b5368c02af2da0bb34738d7baf16c1722865f
JuanRodriguesDev <juanrodrigues.dev@gmail.com>
2022-08-25 14:13:17 -0300

    RecyclerView

--- app/src/main/java/com/project/orgs/MainActivity.kt
+++ app/src/main/java/com/project/orgs/MainActivity.kt
@@ -2,6 +2,7 @@ package com.project.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.renderscript.Sampler
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
@@ -10,8 +11,15 @@ class MainActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = TextView(this)
-        view.setText("Cesta de frutas")
-        setContentView(view)
-     }
+
+        setContentView(R.layout.main_activity)
+
+        val name = findViewById<TextView>(R.id.name)
+        name.text = ("cesta de fruta")
+        val description = findViewById<TextView>(R.id.description)
+        description.text = ("Laranja, Manga e Uva")
+        val value = findViewById<TextView>(R.id.value)
+        value.text = ("R$ 19,99")
+    }
+
 }
