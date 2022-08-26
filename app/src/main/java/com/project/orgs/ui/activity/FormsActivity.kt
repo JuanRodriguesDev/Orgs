9a5f7827f2e14621444ad44343851b27a5a917f3
JuanRodriguesDev <juanrodrigues.dev@gmail.com>
2022-08-26 14:11:41 -0300

    Forms integration

---
+++ app/src/main/java/com/project/orgs/ui/activity/FormsActivity.kt
@@ -0,0 +1,10 @@
+package com.project.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.orgs.R
+
+
+class FormsActivity : AppCompatActivity(R.layout.activity_forms) {
+    }
+
--- app/src/main/java/com/project/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/project/orgs/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.project.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.project.orgs.ListaProdutosAdapter
@@ -9,19 +10,13 @@ import com.project.orgs.Produto
 import com.project.orgs.R
 import java.math.BigDecimal
 
-class MainActivity: Activity() {
+class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.main_activity)
 
-//        val name = findViewById<TextView>(R.id.name)
-//        name.text = ("cesta de fruta")
-//        val description = findViewById<TextView>(R.id.description)
-//        description.text = ("Laranja, Manga e Uva")
-//        val value = findViewById<TextView>(R.id.value)
-//        value.text = ("R$ 19,99")
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
             Produto(nome = "Test",
@@ -31,10 +26,17 @@ class MainActivity: Activity() {
                 descricao = "test desc 2 ",
                 value = BigDecimal("500.00")),
             Produto(nome = "Test 3",
-                descricao = "test desc 3 ",
+                descricao = "test desc",
                 value = BigDecimal("1202.00"))
         ))
 //        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
 }
+
+//        val name = findViewById<TextView>(R.id.name)
+//        name.text = ("cesta de fruta")
+//        val description = findViewById<TextView>(R.id.description)
+//        description.text = ("Laranja, Manga e Uva")
+//        val value = findViewById<TextView>(R.id.value)
+//        value.text = ("R$ 19,99")
